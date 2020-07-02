980dd1687c7c9370e7c43ebb2fd227c984fd1c94
Linawaty <linawatylim@gmail.com>
2020-07-02 21:32:06 +0800

    fix the logic of the diagonal

--- loop-the-loop.js
+++ loop-the-loop.js
@@ -1,30 +1,23 @@
-const arr = [ [10, 20, 30],
-            [40, 50, 60],
-            [70, -80, 90] ];
+const arr = [[10, 20, 30],
+[40, 50, 60],
+[70, -80, 90]];
 
 const bigArray =
-[ [ 887, -541, -430, -590, 117, 172, -319, -18 ],
-  [ -269, 964, 209, 840, -456, 156, 365, -568 ],
-  [ 289, -41, 488, 198, 240, 124, -427, 214 ],
-  [ 452, 894, 968, -149, 683, 977, 741, -805 ],
-  [ 181, -714, -950, 107, 800, 751, -143, 380 ],
-  [ 152, 493, 707, 914, 37, 356, -625, 608 ],
-  [ -345, 906, 83, 676, 723, 381, 557, -183 ],
-  [ 199, -943, -710, 565, 193, 315, 281, 245 ] ];
+    [[887, -541, -430, -590, 117, 172, -319, -18],
+    [-269, 964, 209, 840, -456, 156, 365, -568],
+    [289, -41, 488, 198, 240, 124, -427, 214],
+    [452, 894, 968, -149, 683, 977, 741, -805],
+    [181, -714, -950, 107, 800, 751, -143, 380],
+    [152, 493, 707, 914, 37, 356, -625, 608],
+    [-345, 906, 83, 676, 723, 381, 557, -183],
+    [199, -943, -710, 565, 193, 315, 281, 245]];
 
 const sumArrElement = (inputArr) => {
-    return inputArr.reduce((a, b) => a + b, 0)
+    return inputArr.reduce((a, b) => a + b);
 }
 
 const transposeArr = (matrix) => {
-    for (var i = 0; i < matrix.length; i++) {
-        for (var j = 0; j < i; j++) {
-            const temp = matrix[i][j];
-            matrix[i][j] = matrix[j][i];
-            matrix[j][i] = temp;
-        }
-    }
-    return matrix;
+    return matrix[0].map((col, c) => matrix.map((row, r) => matrix[r][c]));
 }
 
 // Function to get the Max value in Array
@@ -40,38 +33,40 @@ const greatestSum = (inputArr) => {
     const sideLen = inputArr.length - 1;
     const transposedArr = transposeArr(inputArr);
 
+    console.log(transposedArr);
+
     console.log('sideLen: ', sideLen);
     //calc the horizontal sum
     inputArr.forEach((row, idx) => {
-        // console.log(sumArrElement(row));
+        console.log('row: ', sumArrElement(row));
         allSum.push(sumArrElement(row));
-
-        //calc slash diagonal sum
-        const diagArr = [];
-        for (i = 0; i <= sideLen; i++) {
-            if (i === idx) {
-                diagArr.push(row[i]);
-            }
-        }
-        // console.log(sumArrElement(diagArr));
-        allSum.push(sumArrElement(diagArr));
-
-        //calc backslash diagonal sum
-        const diagArr2 = [];
-        for (i = 0; i <= sideLen; i++) {
-            if ((sideLen - i) === idx) {
-                diagArr2.push(row[i]);
-            }
-        }
-        // console.log(sumArrElement(diagArr2));
-        allSum.push(sumArrElement(diagArr2));
     });
 
     //calc the vertical sum
     transposedArr.forEach(row => {
-        // console.log(sumArrElement(row));
+        console.log('tranposed: ', sumArrElement(row));
         allSum.push(sumArrElement(row));
     });
+
+    //calc diagonal sum
+    const diagArr = []; //slash
+    const diagArr2 = []; //backslash
+    inputArr.forEach((row, idx) => {
+        for (i = 0; i <= sideLen; i++) {
+            if (i === idx) {
+                diagArr.push(row[i]);
+            }
+
+            if ((sideLen - i) === idx) {
+                diagArr2.push(row[i]);
+            }
+        }
+    });
+    console.log('slash: ', sumArrElement(diagArr));
+    allSum.push(sumArrElement(diagArr));
+
+    console.log('backslash: ', sumArrElement(diagArr2));
+    allSum.push(sumArrElement(diagArr2));
 
     //display the biggest sum
     console.log('allSum: ', allSum);
