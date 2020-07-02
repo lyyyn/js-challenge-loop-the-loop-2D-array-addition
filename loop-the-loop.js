b14078fdbd711474edd66dfe0f17b59f63d8303d
Linawaty <linawatylim@gmail.com>
2020-07-02 20:27:34 +0800

    calc the vertical sum

--- loop-the-loop.js
+++ loop-the-loop.js
@@ -6,9 +6,21 @@ const sumArrElement = (inputArr) => {
         return inputArr.reduce((a, b) => a + b, 0)
 }
 
+const transposeArr = (matrix) => {
+    for (var i = 0; i < matrix.length; i++) {
+        for (var j = 0; j < i; j++) {
+          const temp = matrix[i][j];
+          matrix[i][j] = matrix[j][i];
+          matrix[j][i] = temp;
+        }
+      }
+      return matrix;
+}
+
 const greatestSum = (inputArr) => {
     const allSum = [];
     const sideLen = inputArr.length;
+    const transposedArr = transposeArr(inputArr);
 
     console.log('sideLen: ',sideLen);
     //calc the horizontal sum
@@ -18,11 +30,9 @@ const greatestSum = (inputArr) => {
     });
 
     //calc the vertical sum
-    inputArr.map(row => {
-        const arrCol = [];
-        
+    transposedArr.forEach(row => {
         // console.log(sumArrElement(row));
-        // allSum.push(sumArrElement(row));
+        allSum.push(sumArrElement(row));
     });
     //calc slash diagonal sum
     //calc backslash diagonal sum
@@ -31,3 +41,5 @@ const greatestSum = (inputArr) => {
 }         
 
 greatestSum(inputArr); //180
+
+console.log(transposeArr(inputArr));
