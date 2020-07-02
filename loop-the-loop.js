5639faf9a04c38654d6de022fa1dc2370294a648
Linawaty <linawatylim@gmail.com>
2020-07-02 20:54:00 +0800

    fix the max for account for negative

--- loop-the-loop.js
+++ loop-the-loop.js
@@ -1,6 +1,12 @@
-const inputArr = [[10, 20, 30],
-[40, 50, 60],
-[70, -80, 90]];
+const inputArr =
+[ [ 887, -541, -430, -590, 117, 172, -319, -18 ],
+  [ -269, 964, 209, 840, -456, 156, 365, -568 ],
+  [ 289, -41, 488, 198, 240, 124, -427, 214 ],
+  [ 452, 894, 968, -149, 683, 977, 741, -805 ],
+  [ 181, -714, -950, 107, 800, 751, -143, 380 ],
+  [ 152, 493, 707, 914, 37, 356, -625, 608 ],
+  [ -345, 906, 83, 676, 723, 381, 557, -183 ],
+  [ 199, -943, -710, 565, 193, 315, 281, 245 ] ];
 
 const sumArrElement = (inputArr) => {
     return inputArr.reduce((a, b) => a + b, 0)
@@ -16,6 +22,14 @@ const transposeArr = (matrix) => {
     }
     return matrix;
 }
+
+// Function to get the Max value in Array
+const maxInArray = (array) => {
+    let sortedArray = [...array].sort(function (a, b) {
+        return b - a; //ASC a-b //DESC b-a
+    })
+    return sortedArray[0];
+};
 
 const greatestSum = (inputArr) => {
     const allSum = [];
@@ -57,7 +71,7 @@ const greatestSum = (inputArr) => {
 
     //display the biggest sum
     console.log('allSum: ', allSum);
-    console.log('maxSum: ', allSum.reverse()[0]);
+    console.log('maxSum: ', maxInArray(allSum));
 }
 
 greatestSum(inputArr); //180
